5c52dd27348c5233ed7c76d8e8d3fd0b2d212487
Syaqirah Sazalee <syaqirahnf@gmail.com>
2021-03-18 19:10:18 +0800

    First commit for Location.java

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,33 @@
+/*
+ *This program will have setter and getter for class Location
+ *author : Syaqirah Sazalee
+ */
+package samples.demo;
+
+public class Location {
+
+	private String state;
+	private String country;
+	
+	//setter and getter for state
+	public void setState(String state)
+	{
+		this.state=state;
+	}
+
+	public String getState()
+	{
+		return state;
+	}
+	
+	//setter and getter for country
+	public void setCountry(String country)
+	{
+		this.country=country;
+	}
+
+	public String getCountry()
+	{
+		return country;
+	}
+}
