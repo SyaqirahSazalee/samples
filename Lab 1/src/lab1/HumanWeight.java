16de8fa12fa69829cddd926fbd9fc15ed65a8e75
Syaqirah Sazalee <syaqirahnf@gmail.com>
2021-03-19 00:07:32 +0800

    lab 1 second commit

--- Lab 1/src/lab1/HumanWeight.java
+++ Lab 1/src/lab1/HumanWeight.java
@@ -2,14 +2,17 @@ package lab1;
 
 public class HumanWeight {
 
+	//attributes declaration
 	protected double weightOnEarth;
 	protected double weightOnMars;
 
+	//constructor of class
 	public HumanWeight (double weightOnEarth) 
 	{
 		this.weightOnEarth=weightOnEarth;
 	}
 
+	//setter and getter for weightOnEarth
 	public void setWeightOnEarth (double weightOnEarth)
 	{
 		this.weightOnEarth=weightOnEarth;
@@ -20,6 +23,7 @@ public class HumanWeight {
 		return weightOnEarth;
 	}
 	
+	//setter and getter for weightOnMars
 	public void setWeightOnMars (double weightOnMars)
 	{
 		this.weightOnMars=weightOnMars;
@@ -30,6 +34,7 @@ public class HumanWeight {
 		return weightOnMars;
 	}
 
+	//method to calculate weight on mars based on weight on earth
 	public void calculateWeightOnMars()
 	{
 		weightOnMars = (getWeightOnEarth() * 9.81) / 3.711;
--- Lab 1/src/lab1/HumanWeightDemo.java
+++ Lab 1/src/lab1/HumanWeightDemo.java
@@ -8,12 +8,20 @@ public class HumanWeightDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		HumanWeight weight1 = new HumanWeight (70);
-		
+		//set weight for person 1 and display the weight on earth and mars
+		HumanWeight weight1 = new HumanWeight (60);
 		weight1.calculateWeightOnMars();
 		
 		System.out.println("Weight on earth: " + weight1.getWeightOnEarth());
 		System.out.println("Weight on mars: " + weight1.getWeightOnMars());
 		
+		System.out.print("\n\n");
+		
+		//set weight for person 2 and display the weight on earth and mars
+		HumanWeight weight2 = new HumanWeight (70);
+		weight2.calculateWeightOnMars();
+		
+		System.out.println("Weight on earth: " + weight2.getWeightOnEarth());
+		System.out.println("Weight on mars: " + weight2.getWeightOnMars());
 	}
 }
