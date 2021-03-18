607ec4f0ccbe1ce4cf2a1f27f89048781928ec32
Syaqirah Sazalee <syaqirahnf@gmail.com>
2021-03-18 18:47:50 +0800

    Initial Commit

---
+++ samples/src/samples/demo/GitTest.java
@@ -0,0 +1,11 @@
+/*
+ *This program will display "Git is fun"
+ *author : Syaqirah Sazalee
+ */
+package samples.demo;
+
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+	}
+}
